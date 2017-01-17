e9b3354e7c1743874ab99c709a3d765626cfa58a
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-01-17 10:51:11 -0800

    Update marvel.py

--- GraphX/marvel.py
+++ GraphX/marvel.py
@@ -1,4 +1,9 @@
-
+# Marvel Social Network:
+# (1)Vertices: characters
+#    - Size of vertice is proportional to the number of times the character appears in any comic book
+# (2)Edges: a relationship between two characters
+#    - Edge weight is proportional to the number of times the characters appear together
+     - co-occurence network
 from graphframes import *
 edgesPath="hdfs:///user/swethakolalapudi/Marvel/Edges.csv"
 verticesPath="hdfs:///user/swethakolalapudi/Marvel/Vertices.csv"
