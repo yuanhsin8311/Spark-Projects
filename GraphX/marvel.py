622421dbb0cca9aef010b748df074df4baaf7181
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-01-17 13:33:12 -0800

    Update marvel.py

--- GraphX/marvel.py
+++ GraphX/marvel.py
@@ -1,9 +1,27 @@
-# Marvel Social Network:
-# (1)Vertices: characters
-#    - Size of vertice is proportional to the number of times the character appears in any comic book
-# (2)Edges: a relationship between two characters
-#    - Edge weight is proportional to the number of times the characters appear together
-     - co-occurence network
+# Marvel Social Network
+
 from graphframes import *
-edgesPath="hdfs:///user/swethakolalapudi/Marvel/Edges.csv"
-verticesPath="hdfs:///user/swethakolalapudi/Marvel/Vertices.csv"
+
+edgesPath = "hdfs:///user/yuanhsin/spark/Marvel/Edges.csv"
+verticesPath = "hdfs:///user/yuanhsin/spark/Marvel/Vertices.csv"
+
+# Use SQLConext to set up the DataFrames
+from pyspark.sql import SQLContext,Row
+sqlC = SQLContext(sc)
+
+edges = sqlC.createDataFrame(sc.textFile(edgesPath)).map(lambda x: x.split(",")).
+edges=sqlC.createDataFrame\
+    (sc.textFile(edgesPath)\
+    .map(lambda x:x.split(","))\
+    .map(lambda x:[int(y) for y in x])\
+    ,["src","dst","wt"])
+    
+vertices=sqlC.createDataFrame\
+    (sc.textFile(verticesPath)\
+    .map(lambda x:x.split("|"))\
+    .map(lambda x:[int(x[0]),x[1],x[2]])\
+    ,["id","name","wt"])
+marvelSocial=GraphFrame(vertices, edges)
+degrees=marvelSocial.degrees
+degrees
+degrees.take(10)
